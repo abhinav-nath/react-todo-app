5449edc3e0c329c7ca88a0fd6cd0aa67cd696587
Abhinav Nath <abhinavnath@ymail.com>
2024-03-07 00:10:06 +0530

    Handle task and status change using useState

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+  const [task, setTask] = useState("");
+  const [status, setStatus] = useState("todo");
+
+  const handleTaskChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
   return (
     <header className="app_header">
       <form>
-        <input type="text" className="task_input" placeholder="Add a task" />
+        <input
+          type="text"
+          className="task_input"
+          placeholder="Add a task"
+          onChange={(e) => handleTaskChange(e)}
+        />
         <div className="task_form_bottom_line">
           <div>
             <Tag tagName="HTML" />
@@ -17,7 +33,7 @@ const TaskForm = () => {
           </div>
 
           <div>
-            <select className="task_status">
+            <select className="task_status" onChange={handleStatusChange}>
               <option value="todo">To Do</option>
               <option value="in-progress">In Progress</option>
               <option value="done">Done</option>
