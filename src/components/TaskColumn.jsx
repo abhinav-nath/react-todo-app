f6b960af92f507560bbe90cf980bd8961373d1e8
Abhinav Nath <abhinavnath@ymail.com>
2024-03-06 23:48:10 +0530

    Destructure props

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./TaskColumn.css";
 
-const TaskColumn = (props) => {
+const TaskColumn = ({ title, icon }) => {
   return (
     <section className="task_column">
       <h2 className="task_column_heading">
-        <img className="task_column_icon" src={props.icon} /> {props.title}
+        <img className="task_column_icon" src={icon} /> {title}
       </h2>
     </section>
   );
